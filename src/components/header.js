fddbcb79826f9a65b0c538ccffa3a5784511c223
David Stein <dstein131@gmail.com>
2022-05-21 19:52:36 -0400

    ok

--- src/components/header.js
+++ src/components/header.js
@@ -27,7 +27,7 @@ function Header() {
         style={{ backgroundColor: "#f7f7f7" }}
       >
         <Container>
-          <Image fluid rounded src={logo} width={260}></Image>
+          <Image fluid rounded src={logo} width={125}></Image>
           {/* <Navbar.Brand href="#home">Murray Hill Design</Navbar.Brand> */}
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
